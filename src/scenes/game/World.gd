05c8282f315c2c710c5059a817fc77eb0bc2976b
Anhel <anhel.strauke@gmail.com>
2020-05-02 00:05:01 +0700

    Added test textures

--- src/scenes/game/World.gd
+++ src/scenes/game/World.gd
@@ -3,10 +3,15 @@ extends Node2D
 
 func _ready():
 	# Assign player #0 with controller #0
-	GameInput.set_gamepad_input_source(0, 0)
+	if GamepadManager.gamepads.size() > 0:
+		GameInput.set_gamepad_input_source(0, 0)
+	else:
+		GameInput.set_keyboard_input_source(0)
 	# Assign player #1 with controller #1
-	GameInput.set_gamepad_input_source(1, 1)
-	
+	if GamepadManager.gamepads.size() > 1:
+		GameInput.set_gamepad_input_source(1, 1)
+	else:
+		GameInput.set_keyboard_input_source(1)
 	# Alternative: assign keyboard controls (WSAD) to player #0
 	# GameInput.set_keyboard_input_source(0)
 	# Alternative: assign keyboard controls (↑↓←→) to player #1
