124879ba5cc6552e3aa994bf6291214612e3e3b6
Anhel <anhel.strauke@gmail.com>
2020-05-03 06:31:42 +0700

    Weapon animations and individual bullets

--- src/scenes/game/character/Weapons/AssaultRifle.gd
+++ src/scenes/game/character/Weapons/AssaultRifle.gd
@@ -1,7 +1,7 @@
 extends "res://scenes/game/character/Weapons/Firearm.gd"
 
 
-const MyBullet = preload("res://scenes/game/character/Weapons/projectiles/GunBullet.tscn")
+const MyBullet = preload("res://scenes/game/character/Weapons/projectiles/AssaultRifleBullet.tscn")
 
 
 func create_bullet() -> Node:
--- src/scenes/game/character/Weapons/Projectile.gd
+++ src/scenes/game/character/Weapons/Projectile.gd
@@ -1,20 +1,37 @@
 extends KinematicBody2D
 
+onready var sound_player = $AudioStreamPlayer
 
 var speed: float = 30.0
 var direction_vector: Vector2 = Vector2.ZERO setget set_direction
 
-const FRICTION = 12.0
+var friction = 12.0
 var _velocity: Vector2 = Vector2.ZERO
+var damage = 1
 var gunslinger_name: String
+var sound: AudioStream = null
 
+
+func set_sound(snd: AudioStream) -> void:
+	sound = snd
+
+
+func _ready() -> void:
+	sound_player.stream = sound
+	remove_child(sound_player)
+	get_tree().root.add_child(sound_player)
+	sound_player.connect("finished", sound_player, "queue_free")
+	sound_player.play()
+	
 
 func set_direction(vect: Vector2) -> void:
 	direction_vector = vect.normalized()
 	_velocity = direction_vector * speed
 
 func _physics_process(delta: float) -> void:
-	_velocity = _velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	_velocity = _velocity.move_toward(Vector2.ZERO, friction * delta)
+	if _velocity.length() < 10:
+		queue_free()
 	var collision = move_and_collide(_velocity)
 	if collision:
 		print(collision.collider.name)
