b789357f93911c2130656e7b949dfab9dd71d718
Anhel <anhel.strauke@gmail.com>
2020-05-02 12:41:53 +0700

    Transition scene added

---
+++ src/scenes/LoadingScene.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+onready var anim_player = $AnimationPlayer
+
+var preloaded_scenes = {}
+
+var next_scene_name: String = ""
+
+onready var background = $Background
+
+func _ready() -> void:
+	background.visible = false
+
+
+func run_scene(scene_resource: String) -> void:
+	assert(next_scene_name == "")
+	next_scene_name = scene_resource
+	anim_player.play("show")
+
+
+func _on_animation_finished(anim_name: String) -> void:
+	match anim_name:
+		"show":
+			var old_scene = get_tree().current_scene
+			if old_scene:
+				old_scene.queue_free()
+			#var preloaded_res = preloaded_scenes.get(next_scene_name, null)
+			#if not preloaded_res:
+			#	preloaded_res = load(next_scene_name)
+			#	preloaded_scenes[next_scene_name] = preloaded_res
+			#var scene = preloaded_res.instance()
+			get_tree().change_scene(next_scene_name)
+			next_scene_name = ""
+			anim_player.play("hide")
+		"hide":
+			pass
--- src/scenes/ui/menu/PlayerSelectSlider.gd
+++ src/scenes/ui/menu/PlayerSelectSlider.gd
@@ -6,6 +6,11 @@ var new_game: bool = true setget set_new_game
 
 func set_new_game(n: bool) -> void:
 	if n:
-		title_label.text = "New Game"
+		title_label.text = "Start Game"
 	else:
 		title_label.text = "Continue Game"
+
+
+
+func _on_SinglePlayer_activated():
+	LoadingScene.run_scene("res://scenes/game/World.tscn")
