4e606127732977d05c41a8d8bed7063711a4b38b
Anhel <anhel.strauke@gmail.com>
2020-05-03 21:12:00 +0700

    Bullets bug fixed (by Scoba)

--- src/scenes/game/character/Weapons/Firearm.gd
+++ src/scenes/game/character/Weapons/Firearm.gd
@@ -31,18 +31,41 @@ func create_bullet() -> Node:
 	return Bullet.instance()
 
 
+#func setup_projectile(projectile) -> void:
+#	projectile.gunslinger_name = owner_name
+#	projectile.set_sound(shoot_sound)
+#	projectile.global_position = bullet_spawn.global_position
+#	projectile.damage = hit_damage
+#	projectile.impulse = knockback_impulse
+#
+#	# position viewport correction:
+#	var vp = get_parent()
+#	while not vp is Viewport:
+#		vp = vp.get_parent()
+#	projectile.global_position += vp.get_parent().global_position - Vector2(150, 200)
+
+
 func setup_projectile(projectile) -> void:
 	projectile.gunslinger_name = owner_name
 	projectile.set_sound(shoot_sound)
-	projectile.global_position = bullet_spawn.global_position
 	projectile.damage = hit_damage
 	projectile.impulse = knockback_impulse
 
-	# position viewport correction:
-	var vp = get_parent()
-	while not vp is Viewport:
-		vp = vp.get_parent()
-	projectile.global_position += vp.get_parent().global_position - Vector2(150, 200)
+#	position viewport correction:
+#	var root = get_tree().get_root()
+#	var cur_node = self
+#	var msg_text = "Positions:\n"
+#	while cur_node != root:
+#		msg_text += cur_node.name + ": "
+#		if not cur_node.get('global_position'):
+#		msg_text += "no global_position"
+#	else:
+#		msg_text += "x=" + str(cur_node.global_position.x) + ", y=" + str(cur_node.global_position.y)
+#	msg_text += "\n"
+#	cur_node = cur_node.get_parent()
+#	root.get_node("World").get_node("Label").text = msg_text
+	var vp = get_parent().get_parent().get_parent()
+	projectile.global_position = bullet_spawn.global_position + vp.get_parent().global_position - Vector2(150, 200)
 
 
 func make_shot(direction: Vector2, parent: Node) -> void:
