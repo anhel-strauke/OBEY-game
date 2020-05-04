0e530c51241963cf6596e1c5a5b449de4e5ae6ff
Scoba <anton@doridaweb.com>
2020-05-04 14:59:03 +0700

    Memory fix

--- src/gfx/scenes/Postprocessing.gd
+++ src/gfx/scenes/Postprocessing.gd
@@ -6,15 +6,24 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	var to_erase = []
 	for item_id in items:
 		var item = items[item_id]['item']
 		var controller = items[item_id]['controller']
 		if is_instance_valid(controller):
-			item.global_position = controller.global_position
-			item.rotation = controller.rotation
+#			item.global_position = controller.global_position
+#			item.rotation = controller.rotation
+			if controller.get("postprocessing_controller_flag"):
+				item.global_position = controller.global_position
+				item.rotation = controller.rotation
+			else:
+				to_erase.append(item_id)
+				item.queue_free()
 		else:
 			item.queue_free()
-			items.erase(item_id)
+			to_erase.append(item_id)
+	for iid in to_erase:
+		items.erase(iid)
 
 var max_id = 0
 var items = {}
--- src/scenes/game/effects/RippleController.gd
+++ src/scenes/game/effects/RippleController.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+export var postprocessing_controller_flag = true
+
 func _ready():
 	var controller = get_tree().get_nodes_in_group("postproc_controller")[0]
 	controller.create_ripple(self)
